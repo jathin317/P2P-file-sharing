d6f56df3c0e216b40daa381ee5e4e79eb46ba74a
jathin317 <jathin317@gmail.com>
2026-06-02 00:30:50 +0530

    added qr_code feature

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for, url_for
 from werkzeug.utils import secure_filename
 from waitress import serve
+import qrcode
+import socket
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -11,9 +13,28 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+def generate_qr_code(data):
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(get_local_ip()+':5000')
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img.save(os.path.join(app.static_folder, 'qr_code.png'))
+
 @app.route('/', methods=['GET'])
 def index():
     files = os.listdir(app.config['UPLOAD_FOLDER'])
+    generate_qr_code('')
     return render_template('index.html', files=files)
 
 @app.route('/upload_chunk', methods=['POST'])
