40962c27ab0a21271d92f3eabe5dc96f219b4865
jathin317 <jathin317@gmail.com>
2026-06-16 15:24:47 +0530

    Refactor app structure: streamline routes, add user login and peer listing, and update HTML templates

--- app.py
+++ app.py
@@ -1,78 +1,24 @@
-import os
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for, url_for
-from werkzeug.utils import secure_filename
-from waitress import serve
-import qrcode
-import socket
-
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+users = {}
 
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    except Exception:
-        ip = '127.0.0.1'
-    finally:
-        s.close()
-    return ip
-
-def generate_qr_code(data):
-    qr = qrcode.QRCode(version=1, box_size=10, border=5)
-    qr.add_data('http://'+get_local_ip()+':5000')
-    qr.make(fit=True)
-    img = qr.make_image(fill_color='white', back_color='black')
-    img.save(os.path.join(app.static_folder, 'qr_code.png'))
-
-@app.route('/', methods=['GET'])
+@app.route("/")
 def index():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    generate_qr_code('')
-    return render_template('index.html', files=files)
+    return render_template("index.html")
 
-@app.route('/upload_chunk', methods=['POST'])
-def upload_chunk():
-    fileName = request.headers.get('File-Name')
-    if not fileName:
-        return 'File-Name is missing', 400
-    
-    fileName = secure_filename(fileName)
-    filePath = os.path.join(app.config['UPLOAD_FOLDER'], fileName)
+@app.route("/user", methods=["GET"])
+def login():
+    username = request.args.get("username")
+    ipaddress = request.remote_addr
+    users[ipaddress] = username
+    print(users)
+    return render_template("usersList.html")
 
-    with open(filePath, 'ab') as file:
-        file.write(request.data)
+@app.route("/peers", methods=["GET"])
+def get_peers():
+    return jsonify({"peers": list(users.values())})
 
-    return 'Chunk uploaded', 200
-
-
-"""
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return 'Failed to submit file'
-    
-    file = request.files['file']
-
-    if file.filename == '':
-        return 'No file selected'
-    
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print(file.filename)
-        return redirect(url_for('index'))
-"""
-    
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
-
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
